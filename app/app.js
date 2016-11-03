544a20ec469018f3e5d19c2be79b977013318ab5
prabhatpathania <prabhatpathania19@gmail.com>
2016-11-03 18:25:06 +0530

    Displaying JSON contents in response

--- app/app.js
+++ app/app.js
@@ -1,12 +1,23 @@
 var express = require('express');
 var app = express();
+var dataFile = require('./data/data.json');
 
-app.get('/', function(req, res){
-    res.send('<h3>Response from Express</h3>');
+app.set('port', process.env.PORT || 3000); //to avoid hardcoding the port number
+app.get('/', function (req, res) {
+    var info = '';
+    dataFile.speakers.forEach(function (item) {
+        info += `<li>
+                    <h2>${item.name}</h2>
+                    <p>${item.summary}</p>
+                </li>`;
+    });
+    res.send(`  <h1>Art Academy Meetups</h1>
+                ${info} `);
 });
 
-app.listen(3000, function(){
-    console.log("Listening to port 3000");
+
+app.listen(app.get('port'), function () {
+    console.log("Listening to port " + app.get('port'));
 });
 
 //var http = require('http');
